2352c4f043748c4d65782fe279b7f923713233f2
Patrick Hüntelmann <patrick.huentelmann@gmail.com>
2022-01-26 22:46:56 +0100

    add db context service to service container

--- backend/Yomu.Api/Program.cs
+++ backend/Yomu.Api/Program.cs
@@ -1,6 +1,10 @@
+using Yomu.Shared.Contexts;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
+
+builder.Services.AddDbContext<YomuContext>();
 
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
